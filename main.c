5a837fde628ba8d743dd0bde7c2ddb1f2c946af8
Marian <44369044+ElevenSpins@users.noreply.github.com>
2021-11-19 18:47:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <math.h> //Damit log() funktioniert
 #include <conio.h> //Damit getch() funktioniert
 #include <windows.h>
+#include <wserovfkdlkelrfkldlökgre.h>
 
 //Umlaute, um diese in der Konsole ausgeben zu können
 #define sss 0xe1 // ß
